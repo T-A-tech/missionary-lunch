112772246af657256861342c244b9e6583adbe55
Bergson Lima <bergson.marques@umode.com.br>
2026-03-21 15:32:34 -0300

    Add admin user creation logic to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+admin_email = ENV.fetch("ADMIN_EMAIL", "admin@almoco.com")
+admin_name  = ENV.fetch("ADMIN_NAME", "Admin")
+admin_pass  = ENV.fetch("ADMIN_PASSWORD", "changeme123")
+
+unless User.exists?(email: admin_email.downcase)
+  User.create!(
+    name: admin_name,
+    email: admin_email,
+    password: admin_pass,
+    password_confirmation: admin_pass
+  )
+  puts "Admin user created: #{admin_email}"
+end
